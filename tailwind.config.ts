4c6d42456904da5a67341a857db17a2a024b6401
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-04 17:53:04 -0500

    tailwind variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)"
+        "primary-100": "#2563eb",
+        "primary-200": "#59a5f5",
+        "primary-300": "#c8ffff",
+        "accent-100": "#00bfff",
+        "accent-200": "#1f50bb",
+        "text-100": "#333333",
+        "text-200": "#5c5c5c",
+        "bg-100": "#fafafa",
+        "bg-200": "#f5f5f5",
+        "bg-300": "#dee5ec",
+        "bg-50": "#ffffff"
       }
     }
   },
