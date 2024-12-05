2cd18b3e0faa87224b3524a30c10b39088b2bd11
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-04 23:09:41 -0500

    Props interface with className

---
+++ src/app/core/interfaces/props.ts
@@ -0,0 +1,6 @@
+// Objetivo: Definir interfaces para props de componentes.
+
+// Interfaz para props de componentes con className opcional.
+export interface PropsWithClassName { 
+  className?: string
+}
