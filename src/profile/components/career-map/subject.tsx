b8616f3b7a27b0a77f8a002dfa7565fd6aa82372
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-10 10:53:18 -0500

    fix: update subject status references to use SUBJECT_STATUS constants

--- src/profile/components/career-map/subject.tsx
+++ src/profile/components/career-map/subject.tsx
@@ -1,4 +1,5 @@
 import { CAREER_MAP_COLORS } from "@/profile/consts/career-map-colors"
+import { SUBJECT_STATUS } from "@/profile/consts/subject-status"
 import { Subject as SubjectType } from "@/profile/interfaces/career-map"
 import clsx from "clsx"
 
@@ -11,9 +12,9 @@ const Subject = ({
 }: Props) => {
   return (
     <li className={clsx(`h-full max-w-[120px] rounded-lg shadow-custom-card py-2.5 px-3 flex items-center justify-center ${className}`, {
-      [CAREER_MAP_COLORS.approved.className]: status === "aprovada",
-      [CAREER_MAP_COLORS.current.className]: status === "cursando",
-      [CAREER_MAP_COLORS.pending.className]: status === "pendiente"
+      [CAREER_MAP_COLORS.approved.className]: status === SUBJECT_STATUS.approved,
+      [CAREER_MAP_COLORS.current.className]: status === SUBJECT_STATUS.current,
+      [CAREER_MAP_COLORS.pending.className]: status === SUBJECT_STATUS.pending
     })}>
       <p className="line-clamp-3 text-center text-xs font-medium">
         {name}
