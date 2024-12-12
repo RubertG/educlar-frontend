a4af316fc5a6f6cc375bb5c1e49fcaad9a8de498
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-12 16:38:15 -0500

    fix: Add tooltip to show subject name on race map

--- src/profile/components/career-map/subject.tsx
+++ src/profile/components/career-map/subject.tsx
@@ -1,3 +1,4 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/core/components"
 import { CAREER_MAP_COLORS } from "@/profile/consts/career-map-colors"
 import { SUBJECT_STATUS } from "@/profile/consts/subject-status"
 import { Subject as SubjectType } from "@/profile/interfaces/career-map"
@@ -11,14 +12,25 @@ const Subject = ({
   name, status, className
 }: Props) => {
   return (
-    <li className={clsx(`h-full max-w-[120px] rounded-lg shadow-custom-card py-2.5 px-3 flex items-center justify-center ${className}`, {
+    <li className={clsx(`h-full max-w-[125px] rounded-lg shadow-custom-card py-2.5 px-3 flex items-center justify-center ${className}`, {
       [CAREER_MAP_COLORS.approved.className]: status === SUBJECT_STATUS.approved,
       [CAREER_MAP_COLORS.current.className]: status === SUBJECT_STATUS.current,
       [CAREER_MAP_COLORS.pending.className]: status === SUBJECT_STATUS.pending
     })}>
-      <p className="line-clamp-3 text-center text-xs font-medium">
-        {name}
-      </p>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <p className="line-clamp-3 text-center text-xs font-medium break-words">
+              {name}
+            </p>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>
+              {name}
+            </p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </li >
   )
 }
