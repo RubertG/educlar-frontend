242be431aadbf411b1e20c6d0ed1d55bc8f63200
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-13 01:51:37 -0500

    feat: Añadir configuración para permitir imágenes desde un servidor local

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,15 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5400'
+      }
+    ]
+  }
 }
  
 export default nextConfig
